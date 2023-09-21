21682834bc60fe430d34bd5baae033b7b371cfd4
Cody Marshall <codymstuntsx@gmail.com>
2023-09-20 18:16:21 -0700

    UPDATED: changed li's to next LINK

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -12,8 +12,13 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 right-0 left-0 flex justify-between transparent text-white text-4xl p-2">
-      <Link href="/">Better Reads</Link>
-      <button onClick={handleOpenNav}>Menu</button>
+      <Link href="/">Bettereads</Link>
+      <button
+        className="hover:text-[#9015D5] transition 300 ease-in"
+        onClick={handleOpenNav}
+      >
+        Menu
+      </button>
       {showNavSlider && (
         <div className="fixed inset-y-0 right-0 h-screen w-96 bg-black transition 500 ease-in-out">
           <div className="flex justify-end">
@@ -23,9 +28,20 @@ export default function Navbar() {
           </div>
 
           <ul className="flex flex-col gap-4 p-4">
-            <li>Home</li>
-            <li>News</li>
-            <li>Trending</li>
+            <Link
+              className="hover:text-[#9015D5] text-9xl transition 300 ease-in"
+              onClick={handleOpenNav}
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className="hover:text-[#9015D5] text-9xl transition 300 ease-in"
+              onClick={handleOpenNav}
+              href="/news"
+            >
+              News
+            </Link>
           </ul>
         </div>
       )}
