ff0ee1f1b94f64a13ae6e60a0efd91e24e64c311
Cody Marshall <codymstuntsx@gmail.com>
2023-09-20 18:15:47 -0700

    CREATED: hook to handle article fetching

---
+++ hooks/getArticles.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function getArticles(topic: any, page: number) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<string[] | any>([]);
+  const [error, setError] = useState<any>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get(
+          `https://newsapi.org/v2/top-headlines?language=en&category=${topic}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+        );
+        setData(response);
+        setLoading(false);
+      } catch (error: any) {
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+}
